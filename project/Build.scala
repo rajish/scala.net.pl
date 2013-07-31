fc5faa46bb873e3d0493a3a2adf3630746f5cd48
Maciej Rzepiński <maciej.rzepinski@gmail.com>
2013-07-31 09:02:03 +0200

    salat dependency & configuration

--- project/Build.scala
+++ project/Build.scala
@@ -8,16 +8,18 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
     "com.typesafe.akka" %% "akka-actor" % "2.2.0",
     "com.typesafe.akka" %% "akka-testkit" % "2.2.0" % "test",
-    "com.typesafe.slick" %% "slick" % "1.0.1",
+    "se.radley" % "play-plugins-salat_2.10" % "1.2",
     "org.slf4j" % "slf4j-api" % "1.7.2",
     "securesocial" %% "securesocial" % "master-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+    resolvers += "Typesafe Repository 2" at "http://repo.typesafe.com/typesafe/repo/",
+    resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns),
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId"
   )
 
 }
