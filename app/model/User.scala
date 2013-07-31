90c22b01db5c51ce891aeca0b5f78761e2cfd183
Maciej Rzepiński <maciej.rzepinski@gmail.com>
2013-07-31 09:26:37 +0200

    User model

--- app/model/User.scala
+++ app/model/User.scala
@@ -1,4 +1,29 @@
 package model
 
-case class User(name: String, email: String, githubUsername: String, 
-    avatarUrl: String, isAdmin: Boolean = false, isBlocked: Boolean = false)
+import play.api.Play.current
+import java.util.Date
+import com.novus.salat._
+import com.novus.salat.annotations._
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import se.radley.plugin.salat._
+import mongoContext._
+
+case class User(
+  id: ObjectId = new ObjectId,
+  username: String,
+  password: String,
+  email: String,
+  githubUsername: String,
+  avatarUrl: String,
+  isAdmin: Boolean = false,
+  isBlocked: Boolean = false,
+  createdAt: Option[Date] = None,
+  updated: Option[Date] = None
+)
+
+object User extends ModelCompanion[User, ObjectId] {
+  val dao = new SalatDAO[User, ObjectId](collection = mongoCollection("users")) {}
+
+  def findOneByUsername(username: String): Option[User] = dao.findOne(MongoDBObject("username" -> username))
+}
