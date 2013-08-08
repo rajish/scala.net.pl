41b8adabb70af86a700719082f0b920ccf59fce7
Radzisław Galler <rgaller@gazeta.pl>
2013-08-09 00:59:29 +0200

    New style of writing test specs.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,7 +1,10 @@
 package test
 
 import org.specs2.mutable._
+import org.specs2.specification.{ Scope, Around}
+import org.specs2.execute._
 
+import play.api._, mvc._
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -11,17 +14,29 @@ import play.api.test.Helpers._
  * For more information, consult the wiki.
  */
 class ApplicationSpec extends Specification {
-  
+
   "Application" should {
-    
-    "render the index page" in {
+
+    "render the index page" in  new FakeApp{
       running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
-        
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
       }
     }
-    
+
+  }
+}
+
+trait FakeApp extends WithApplication {
+  lazy val home = route(FakeRequest(GET, "/")).get
+
+  def setup: Unit = {
+    Logger.debug("----- new test -------")
+    // can be some database setup here, etc.
+  }
+
+  override def around[T : AsResult](t: => T) = super.around {
+    setup
+    t
   }
 }
